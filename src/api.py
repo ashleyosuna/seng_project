54e85a80b10bb4fd32d856c1b220836c457788dc
Ashley Osuna <ashleyosuna@uvic.ca>
2025-02-28 10:51:39 -0800

    refactored, labeling as probability instead of binary

--- src/api.py
+++ src/api.py
@@ -1,74 +1,91 @@
 import requests
 import re
 import time
-import datetime
 
 BASE_URL = 'https://arctic-shift.photon-reddit.com/'
 # TODO: change to actual number
-MIN_NUM_POSTS = 200
-START_DATE = int((datetime.datetime(2024, 1, 1) - datetime.datetime(1970, 1, 1)) / datetime.timedelta(seconds=1))
+NUM_SAMPLES = 10
+START_DATE = '2024-01-01'
+VOTE_REGEX = r'(YTA|NTA)'
+
+def get_comments(post_id):
+    search_params = {'link_id': post_id}
+
+    res = requests.get(BASE_URL + '/api/comments/search', params=search_params)
+
+    if res.status_code != 200:
+        print('Error getting comments')
+        return []
+
+    comments = res.json()['data']
+
+    return comments
+
+def get_score(comments):
+    num_positive = 0
+    num_negative = 0
+
+    for comment in comments:
+        vote_match = re.search(VOTE_REGEX, comment['body'], re.IGNORECASE)
+
+        if not vote_match:
+            continue
+        
+        ups = comment['ups']
+
+        if vote_match.group() == 'NTA':
+            num_negative += 1 + ups
+        elif vote_match.group() == 'YTA':
+            num_positive += 1 + ups
+
+    if num_positive == 0 and num_negative == 0:
+        return -1
+
+    score = num_positive / (num_negative + num_positive)
+
+    # rounding to two decimal places
+    score = (round(score * 100)) / 100
+    
+    return score
+
 
 # SEEMS LIKE LIMIT IS 100
-search_params = {'subreddit': 'AmItheAsshole', 'after': START_DATE, 'limit': 100, 'sort': 'asc'}
+# TODO: change to 100
+search_params = {'subreddit': 'AmItheAsshole', 'after': START_DATE, 'limit': 1, 'sort': 'asc'}
 
 posts = []
 
-while len(posts) < MIN_NUM_POSTS:
+while len(posts) < NUM_SAMPLES:
     res = requests.get(BASE_URL + 'api/posts/search', params=search_params)
 
     if res.status_code != 200:
         print('Error retrieving posts')
         exit(0)
-    
-    posts += res.json()['data']
 
-    # ensure we do not get repeated posts
-    last_date = posts[-1]['created_utc']
-    search_params['after'] = last_date + 1
+    res_posts = res.json()['data']
 
-    # rate limiting for the api
-    time.sleep(1)
-
-# filter out posts that have their content removed
-posts = [post for post in posts if post['selftext'] != '[removed]']
-
-# FILTERING OF POSTS
-
-# KEEP X% MOST POPULAR POSTS
-PERCENTAGE = 0.1
-posts = sorted(posts, key=lambda d: d['ups'], reverse=True)[:round(len(posts) * 0.1)]
-
-# CLEAN UP TEXT? REMOVE NEW LINES AND OTHER CHARACTERS
-
-# LABELING DATA
-data = []
-labels = {'NTA': 0, 'YTA': 1}
-for post in posts:
-    id = post['id']
-
-    search_params = {'link_id': id}
-
-    res = requests.get(BASE_URL + '/api/comments/search', params=search_params)
-
-    if (res.status_code == 200):
-        comments = res.json()['data']
-
-        comments = sorted(comments, key=lambda d: d['ups'], reverse=True)
-
-        # ignore posts with no comments or comments without any upvotes
-        if len(comments) == 0 or comments[0]['ups'] == None or comments[0]['ups'] == 0:
-            break
-
-        for comment in comments:
-            text = comment['body']
+    for post in res_posts:
+        # ignore posts with no comments or that have been removed
+        if post['num_comments'] == None or post['num_comments'] == 0 or post['selftext'] == '[removed]':
+            continue
         
-            m = re.search('(NTA|YTA)', text)
+        time.sleep(1)
+        comments = get_comments(post['id'])
+        score = get_score(comments)
 
-            if m:
-                label = labels[m.group()]
-                post_data = {'title': post['title'], 'text': post['selftext'], 'label': label}
-                data.append(post_data)
-                break
+        if score != -1:
+            title = post['title']
+            text = post['selftext']
 
+            data = {'title': title, 'text': text, 'label': score}
+            
+            posts.append(data)
+
+    # avoid getting repeated posts
+    search_params['after'] = res_posts[-1]['created_utc'] + 1
     time.sleep(1)
+
+# for post in posts:
+#     print(post, '\n\n')
+
 
