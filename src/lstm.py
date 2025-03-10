dd8cab898cf0c5b0468b05ef912d6181f7184d4e
Ashley Osuna <ashleyosuna@uvic.ca>
2025-03-09 18:12:56 -0700

    proposed changes to lstm file

--- src/lstm.py
+++ src/lstm.py
@@ -13,11 +13,13 @@ data_df = data_df.sample(frac = 1) # shuffle rows
 X = data_df.iloc[:, :-1]
 y = data_df.iloc[:, -1]
 trainX, testX, trainY, testY = train_test_split(X, y, test_size=0.2)
+testYcpy = testY
 
 trainX = torch.tensor(trainX.values, dtype=torch.float32)
-trainX = trainX.unsqueeze(-1) # reshaping so it has [batch_size, sequence_length, input_size], which is needed by the model
+trainX = trainX.unsqueeze(1) # reshaping so it has [batch_size, sequence_length, input_size], which is needed by the model
 testX = torch.tensor(testX.values, dtype=torch.float32)
-testX = testX.unsqueeze(-1) 
+testX = testX.unsqueeze(1) 
+print(trainX.size())
 
 trainY = torch.tensor(trainY.values, dtype=torch.float32)
 trainY = trainY.unsqueeze(-1)
@@ -29,7 +31,7 @@ class LSTMModel(nn.Module):
         super(LSTMModel, self).__init__()
         self.hidden_dim = hidden_dim
         self.layer_dim = layer_dim
-        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
+        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=False)
         self.fc = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x, h0=None, c0=None):
@@ -43,19 +45,19 @@ class LSTMModel(nn.Module):
             cn: The cell state of the LSTM after processing the input sequence.
         '''
         if h0 is None or c0 is None:
-            h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).to(x.device)
-            c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).to(x.device)
+            h0 = torch.zeros(self.layer_dim, x.size(1), self.hidden_dim).to(x.device)
+            c0 = torch.zeros(self.layer_dim, x.size(1), self.hidden_dim).to(x.device)
         
         out, (hn, cn) = self.lstm(x, (h0, c0)) # passes x, h0, c0 through the lstm layer
-        out = self.fc(out[:, -1, :]) # selects the output of the last time step
+        out = self.fc(out[:,-1,:]) # selects the output of the last time step
         return out, hn, cn
 
 
-model = LSTMModel(input_dim=1, hidden_dim=100, layer_dim=1, output_dim=1)
+model = LSTMModel(input_dim=trainX.size(2), hidden_dim=100, layer_dim=1, output_dim=1)
 criterion = nn.MSELoss() # mean squared error for the loss function
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
-num_epochs = 20 # epochs are number of rounds of training
+num_epochs = 100 # epochs are number of rounds of training
 h0, c0 = None, None
 
 loss_values = []
@@ -64,7 +66,6 @@ for epoch in range(num_epochs):
     optimizer.zero_grad()
 
     outputs, h0, c0 = model(trainX, h0, c0)
-
     loss = criterion(outputs, trainY)
     loss.backward() # backpropagating the error to update the weights
     optimizer.step()
@@ -82,8 +83,19 @@ for epoch in range(num_epochs):
 model.eval()
 test_outputs, _, _ = model(testX) # only one forward pass, no backpropagation to update weights anymore
 mse_loss = nn.MSELoss()
+test_outputs.size()
 test_mse = mse_loss(test_outputs, testY)
 print(f"Mean Squared Error on the Test Set: {test_mse}")
+
+plt.figure(figsize=(12, 6))
+plt.plot(range(200), testYcpy, label='Original Data')
+plt.plot(range(200), test_outputs.detach().numpy(), label='Predicted Data', linestyle='--')
+plt.title('LSTM Model Predictions vs. Original Data')
+plt.xlabel('Time Step')
+plt.ylabel('Value')
+plt.legend()
+plt.show()
+plt.close()
 
 # plotting the training loss
 plt.plot(range(1, num_epochs + 1), loss_values, color='b', label='Training Loss')
